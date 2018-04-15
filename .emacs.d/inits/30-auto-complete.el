0c3ea192677bbffc140689153b900fa6b0633cf8
benevolent0505 <mikio.fjt@gmail.com>
2018-04-15 17:30:01 +0900

    auto-completeを使えるように

--- .emacs.d/inits/30-auto-complete.el
+++ .emacs.d/inits/30-auto-complete.el
@@ -1,9 +1,19 @@
 ;; for perl-completion
-(el-get-bundle auto-complete
+(el-get-bundle auto-complete/fuzzy-el)
+(el-get-bundle auto-complete/popup-el)
+
+(el-get-bundle! auto-complete-config in auto-complete
+  (ac-config-default)
   (ac-set-trigger-key "TAB")
-  (setq ac-auto-start nil
+
+  (add-to-list 'ac-dictionary-directories (locate-user-emacs-file "ac-dict"))
+
+  (setq
     ac-auto-show-menu 0.02
-    ac-menu-map t)
+    ac-menu-map t
+    ac-use-fuzzy t
+    ac-use-comphist t)
+
   (let ((map ac-complete-mode-map))
     (define-key map (kbd "C-n") 'ac-next)
     (define-key map (kbd "C-p") 'ac-previous)
