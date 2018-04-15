e1517cdda4ac3a2662e2c6fbd274cd3210874819
benevolent0505 <mikio.fjt@gmail.com>
2018-04-15 14:20:46 +0900

    autocomplete設定見直し

--- .emacs.d/inits/30-auto-complete.el
+++ .emacs.d/inits/30-auto-complete.el
@@ -1,11 +1,10 @@
 ;; for perl-completion
-(el-get-bundle! auto-complete-config in auto-complete
-  (ac-config-default)
-
-  (add-to-list 'ac-modes 'cperl-mode)
-
+(el-get-bundle auto-complete
   (ac-set-trigger-key "TAB")
-  (setq ac-use-menu-map t
-    ac-use-fuzzy t
-    ac-use-comphist t
-    ac-auto-show-menu 0.02))
+  (setq ac-auto-start nil
+    ac-auto-show-menu 0.02
+    ac-menu-map t)
+  (let ((map ac-complete-mode-map))
+    (define-key map (kbd "C-n") 'ac-next)
+    (define-key map (kbd "C-p") 'ac-previous)
+    (define-key map (kbd "TAB") nil)))
