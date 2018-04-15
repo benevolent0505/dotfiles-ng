83322c0a80c2f2ac557c309adfadfea1532eaf55
benevolent0505 <mikio.fjt@gmail.com>
2018-04-15 21:43:29 +0900

    web-mode導入

---
+++ .emacs.d/inits/40-web-model.el
@@ -0,0 +1,23 @@
+(el-get-bundle! web-mode
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tt\\'" . web-mode))
+
+  (defun web-mode-hook ()
+    "Hooks for web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-style-padding 1)
+    (setq web-mode-script-padding 1)
+    (setq web-mode-block-padding 0))
+
+  (setq web-mode-engines-alist
+    '(("template-toolkit" . "\\.html?\\'" )))
+  (add-hook 'web-mode-hook 'web-mode-hook))
