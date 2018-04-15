db1d183a52f028658427df72438e85e004ab8cb4
benevolent0505 <mikio.fjt@gmail.com>
2018-04-15 17:29:37 +0900

    一旦companyを無効に

--- .emacs.d/inits/30-completion.el
+++ .emacs.d/inits/30-completion.el
@@ -1,20 +1,20 @@
-(el-get-bundle! company
-  :type github :pkgname "company-mode/company-mode"
-  (add-hook 'after-init-hook 'global-company-mode)
+;; (el-get-bundle! company
+;;   :type github :pkgname "company-mode/company-mode"
+;;   (add-hook 'after-init-hook 'global-company-mode)
 
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 2)
-  (setq company-selection-wrap-around t)
+;;   (setq company-idle-delay 0)
+;;   (setq company-minimum-prefix-length 2)
+;;   (setq company-selection-wrap-around t)
 
-  (with-eval-after-load-feature 'company
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (define-key company-search-map (kbd "C-n") 'company-select-next)
-    (define-key company-search-map (kbd "C-p") 'company-select-previous)
-    (define-key company-active-map (kbd "C-m") 'company-complete-selection)
+;;   (with-eval-after-load-feature 'company
+;;     (define-key company-active-map (kbd "C-n") 'company-select-next)
+;;     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+;;     (define-key company-search-map (kbd "C-n") 'company-select-next)
+;;     (define-key company-search-map (kbd "C-p") 'company-select-previous)
+;;     (define-key company-active-map (kbd "C-m") 'company-complete-selection)
 
-    (define-key company-active-map (kbd "C-s") 'company-filter-candidates))
+;;     (define-key company-active-map (kbd "C-s") 'company-filter-candidates))
 
-  (custom-set-variables
-    '(package-selected-packages (quote (company))))
-  (custom-set-faces))
+;;   (custom-set-variables
+;;     '(package-selected-packages (quote (company))))
+;;   (custom-set-faces))
