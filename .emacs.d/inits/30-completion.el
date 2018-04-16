2ddd264cb23f2d3a884df2ff8cd214583ade467c
benevolent0505 <mikio.fjt@gmail.com>
2018-04-17 08:25:45 +0900

    company 設定拡充
    - キーバインド
    - quickhelp
    - emoji

--- .emacs.d/inits/30-completion.el
+++ .emacs.d/inits/30-completion.el
@@ -6,15 +6,27 @@
   (setq company-minimum-prefix-length 2)
   (setq company-selection-wrap-around t)
 
-  (with-eval-after-load-feature 'company
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    (define-key company-search-map (kbd "C-n") 'company-select-next)
-    (define-key company-search-map (kbd "C-p") 'company-select-previous)
-    (define-key company-active-map (kbd "C-m") 'company-complete-selection)
+  (global-set-key (kbd "C-M-i") 'company-complete)
 
-    (define-key company-active-map (kbd "C-s") 'company-filter-candidates))
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-m") 'company-complete-selection)
+
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+
+  ;; TABで候補を設定
+  (define-key company-active-map (kbd "C-i") 'company-complete-selection)
 
   (custom-set-variables
     '(package-selected-packages (quote (company))))
   (custom-set-faces))
+
+(el-get-bundle company-quickhelp
+  :depends pos-tip
+  (with-eval-after-load-feature 'company
+    (company-quickhelp-mode +1)))
+
+(el-get-bundle! company-emoji
+  (add-to-list 'company-backends 'company-emoji))
