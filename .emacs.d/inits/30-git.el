100c8becc9e584140a7773c206aa5e4e4e422bf7
benevolent0505 <mikio.fjt@gmail.com>
2018-04-17 04:45:52 +0900

    git-gutter+に以降 & git-messengerやめる

--- .emacs.d/inits/30-git.el
+++ .emacs.d/inits/30-git.el
@@ -1,17 +1,9 @@
 (el-get-bundle magit
   (global-set-key (kbd "C-x g") 'magit-status))
 
-(el-get-bundle! git-gutter
-  (global-git-gutter-mode +1)
+(el-get-bundle! git-gutter+
+  (global-git-gutter+-mode)
 
   ;; Jump to next/previous hunk
-  (global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
-  (global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
-
-  (custom-set-variables
- '(git-gutter:update-interval 2)))
-
-(el-get-bundle! git-messenger
-  (global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
-  (custom-set-variables
-    '(git-messenger:use-magit-popup t)))
+  (global-set-key (kbd "C-x p") 'git-gutter+-previous-hunk)
+  (global-set-key (kbd "C-x n") 'git-gutter+-next-hunk))
