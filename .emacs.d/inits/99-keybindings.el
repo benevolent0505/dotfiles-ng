8362697207993f2a086346989df29234318fb14d
benevolent0505 <mikio.fjt@gmail.com>
2018-04-24 22:11:27 +0900

    C-oの改行事故を防ぐ

--- .emacs.d/inits/99-keybindings.el
+++ .emacs.d/inits/99-keybindings.el
@@ -1,3 +1,4 @@
 (keyboard-translate ?\C-h ?\C-?) ;; C-hをbackspace同等に
 (global-set-key (kbd "C-x C-o") 'find-file-other-window)
 (global-set-key (kbd "M-ESC ESC") 'keyboard-quit)
+(global-set-key (kbd "C-o") 'keyboard-quit)
