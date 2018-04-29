9a37663312be0d8e7fcdab91ca86e5d2946a00af
benevolent0505 <mikio.fjt@gmail.com>
2018-04-29 19:40:28 +0900

    auto-programmingにキーバインド設定

--- .emacs.d/inits/30-auto-programming.el
+++ .emacs.d/inits/30-auto-programming.el
@@ -1,1 +1,2 @@
-(el-get-bundle! auto-programming in hitode909/emacs-auto-programming)
+(el-get-bundle! auto-programming in hitode909/emacs-auto-programming
+  (global-set-key (kbd "C-x C-a") 'auto-programming))
