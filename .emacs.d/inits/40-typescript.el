50c29f1ca096b4d19ee08965f2e4d2e103c9210d
benevolent0505 <mikio.fjt@gmail.com>
2018-04-29 20:10:17 +0900

    typescript-mode追加 && tideをhookに追加

--- .emacs.d/inits/40-typescript.el
+++ .emacs.d/inits/40-typescript.el
@@ -1,25 +1,31 @@
+(el-get-bundle! typescript-mode
+  (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode)))
+
 (el-get-bundle! tide
-  :depends s
-
+  :depends (s)
   ;; aligns annotation to the right hand side
-  (setq company-tooltip-align-annotations t)
+  (setq company-tooltip-align-annotations t))
 
-  (with-eval-after-load-feature 'web-mode
-    (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-    (add-hook 'web-mode-hook
-      (lambda ()
-        (when (string-equal "tsx" (file-name-extension buffer-file-name))
-          (setup-tide-mode))))
 
-    ;; enable typescript-tslint checker
-    (flycheck-add-mode 'typescript-tslint 'web-mode))
+(with-eval-after-load-feature (web-mode flycheck)
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+    (lambda ()
+      (when (string-equal "tsx" (file-name-extension buffer-file-name))
+        (setup-tide-mode))))
 
-  ;; formats the buffer before saving
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
+;; setup
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+
+(with-eval-after-load-feature (typescript-mode tide flycheck)
   (add-hook 'before-save-hook 'tide-format-before-save)
-  (add-hook 'typescript-mode-hook
-    '(lambda ()
-       (tide-setup)
-       (flycheck-mode +1)
-       (setq flycheck-check-syntax-automatically '(save mode-enabled))
-       (eldoc-mode +1)
-       (tide-hl-identifier-mode +1))))
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
