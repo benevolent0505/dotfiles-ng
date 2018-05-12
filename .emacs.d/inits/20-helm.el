68d723fbaa7377960cbaf262a3702a464065a8e0
benevolent0505 <mikio.fjt@gmail.com>
2018-05-12 19:16:30 +0900

    helm-execute-persistent-actionがglobalでTABで有効になっていた

--- .emacs.d/inits/20-helm.el
+++ .emacs.d/inits/20-helm.el
@@ -1,7 +1,7 @@
 (el-get-bundle helm
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
-  (global-set-key (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
   (global-set-key (kbd "C-x C-r") 'helm-recentf)
 
