ed62079f3a51cfb6a6305b9887ce16e03ef0061c
benevolent0505 <mikio.fjt@gmail.com>
2018-12-31 11:19:20 +0900

    [emacs] javascript設定を追加

--- .emacs.d/inits/40-javascript.el
+++ .emacs.d/inits/40-javascript.el
@@ -5,3 +5,9 @@
     js-indent-level 2)
 
   (add-hook 'js-mode-hook 'js2-minor-mode))
+
+(el-get-bundle! tern
+  (add-hook 'js-mode-hook (lambda () (tern-mode t))))
+
+(el-get-bundle! company-tern
+  (add-to-list 'company-backends 'company-tern))
