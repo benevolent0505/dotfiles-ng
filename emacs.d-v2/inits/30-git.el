590e2f1808260ffd3b302ee08ebf53aec603ab06
benevolent0505 <mikio.fjt@gmail.com>
2019-02-16 20:35:06 +0900

    counsel-ghq 導入

--- emacs.d-v2/inits/30-git.el
+++ emacs.d-v2/inits/30-git.el
@@ -1,3 +1,6 @@
 (el-get-bundle magit
   (setq-default magit-completing-read-function 'ivy-completing-read)
   (global-set-key (kbd "C-x g") 'magit-status))
+
+(el-get-bundle windymelt/counsel-ghq
+  (global-set-key (kbd "C-x C-g") 'counsel-ghq))
