6976bdb40be7eb9ebc2a8d886e7f6bebc0a86584
Mikio Fujita <mikio.fjt@gmail.com>
2019-06-05 00:43:55 +0900

    el-getで特別recipes使ってないので設定消す

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,11 +6,10 @@
 (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (url-retrieve-synchronously
+      "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
     (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path (locate-user-emacs-file "recipes"))
 
 ;; 無いと勝手に挿入されるので
 (package-initialize)
