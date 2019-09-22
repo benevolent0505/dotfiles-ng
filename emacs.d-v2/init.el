cdd44f072dd7d5d4febeba380a69f5dc25bcdbec
Mikio Fujita <mikio.fjt@gmail.com>
2019-09-23 00:27:01 +0900

    open-junk-fileの件は一旦これで解決にする

--- emacs.d-v2/init.el
+++ emacs.d-v2/init.el
@@ -211,11 +211,10 @@
   (add-to-list 'yas-snippet-dirs (locate-user-emacs-file "el-get/yasnippet-snippets/snippets")))
 
 
-;; 挙動が変なのでそのまま open-junk-file 実行後は RET を押してファイルを作る
-;; TODO: ivy との噛み合わせで意図しない挙動になっている気がするので調べる
-(el-get-bundle! emacswiki:open-junk-file
-  (setq-default open-junk-file-format "~/local/junk/%Y/%m/%Y-%m-%d-%H%M%S.org"))
-
+;; ivyだと拡張子を入力しようとすると絞り込みになってしまうので,
+;; open-junk-file 実行後は RET を押してファイルを作る
+(el-get-bundle! open-junk-file
+  (setq-default open-junk-file-format "~/local/tmp/%Y/%m/%Y-%m-%d-%H%M%S-.org"))
 
 (el-get-bundle markdown-mode
   (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
