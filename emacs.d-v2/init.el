04640668ebdd88e6495203b7b0dea99867033c66
Mikio Fujita <mikio.fjt@gmail.com>
2020-02-25 00:20:06 +0900

    Perlのシンタックスハイライト追加

--- emacs.d-v2/init.el
+++ emacs.d-v2/init.el
@@ -341,6 +341,32 @@
 (el-get-bundle ocp-indent)
 
 
+;; Perl
+(el-get-bundle! cperl-mode
+  (defalias 'perl-mode 'cperl-mode)
+
+  (setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+  (setq auto-mode-alist (cons '("\\.cgi$" . cperl-mode) auto-mode-alist))
+  (setq auto-mode-alist (cons '("\\.psgi$" . cperl-mode) auto-mode-alist))
+  (setq auto-mode-alist (cons '("cpanfile$" . cperl-mode) auto-mode-alist))
+
+  (setq-default cperl-indent-level 4
+                cperl-continued-statement-offset 4
+                cperl-close-paren-offset -4
+                cperl-comment-column 119
+                cperl-highlight-variables-indiscriminately t
+                cperl-indent-parens-as-block t
+                cperl-indent-subs-specially nil
+                cperl-label-offset -4
+                cperl-tab-always-indent t
+                cperl-font-lock t
+                cperl-break-one-line-blocks-when-indent nil
+                cperl-fix-hanging-brace-when-indent     nil
+                cperl-merge-trailing-else               nil
+                cperl-indent-region-fix-constructs      nil
+                cperl-max-help-size 119))
+
+
 ;; LSP Clinet
 (el-get-bundle! lsp-mode
   (defun my-lsp-mode-hook ()
