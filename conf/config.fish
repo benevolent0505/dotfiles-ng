a510d31974dd35514a911f215b4ac0599a5b7474
Mikio Fujita <mikio.fjt@gmail.com>
2020-05-04 19:30:14 +0900

    必要そうなものだけ復活

---
+++ conf/config.fish
@@ -0,0 +1,19 @@
+# alias
+alias emacs "emacsclient -nw"
+
+# 便利スクリプト置場
+set -x PATH $PATH $HOME/local/bin
+
+# Perl
+set -x PATH $HOME/.plenv/bin $PATH
+set -x PATH $HOME/.plenv/shims $PATH
+alias reply "rlwrap reply"
+
+# Go
+set -x GOPATH $HOME/develop
+set -x PATH $PATH $GOPATH/bin
+
+# Python
+status --is-interactive; and source (pyenv init -|psub)
+
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
