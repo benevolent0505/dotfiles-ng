36f0675cb811225648214b1f7baa8b6c5aa8027d
benevolent0505 <mikio.fjt@gmail.com>
2020-10-03 21:56:13 +0900

    melpa-stableじゃなくていい

--- emacs.d-v2/init.el
+++ emacs.d-v2/init.el
@@ -17,8 +17,6 @@
 
 ;; package.el
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ;; Appearance
