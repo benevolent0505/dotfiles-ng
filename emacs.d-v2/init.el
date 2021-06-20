601eba21fc6d8b5dd01b8ae38df65eacbf40eb37
benevolent0505 <mikio.fjt@gmail.com>
2021-06-20 11:24:37 +0900

    package settings

--- emacs.d-v2/init.el
+++ emacs.d-v2/init.el
@@ -17,6 +17,11 @@
 
 ;; package.el
 (require 'package)
+(setq package-archives
+  '(("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/")
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-refresh-contents)
 (package-initialize)
 
 
