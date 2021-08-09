175faea8cfb2a5e5e417ca7909afa782d0e59f0f
benevolent0505 <mikio.fjt@gmail.com>
2021-08-09 10:02:00 +0900

    update go settings

--- emacs.d-v2/init.el
+++ emacs.d-v2/init.el
@@ -413,10 +413,17 @@
 
 
 ;; Go
-(el-get-bundle go-mode
-  (setq-default gofmt-command "goimports")
-  (add-hook 'before-save-hook #'gofmt-before-save)
-  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
+(el-get-bundle go-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(lsp-register-custom-settings
+ '(("gopls.completeUnimported" t t)
+   ("gopls.staticcheck" t t)))
 
 
 ;; Perl
